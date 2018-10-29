f421626efd1763393e633c1a64143051259bcd23
Electroshockist <ericonfire2@yahoo.com>
2018-10-29 18:44:01 -0400

    All pieces set on board

--- Chess/src/chess/Board.java
+++ Chess/src/chess/Board.java
@@ -1,11 +1,13 @@
 package chess;
 public class Board {
+    BlackPiece blackPawns[] = new BlackPiece[8];
     BlackPiece blackRook[] = new BlackPiece[2];
     BlackPiece blackKnight[] = new BlackPiece[2];
     BlackPiece blackBishop[] = new BlackPiece[2];
     BlackPiece blackQueen;
     BlackPiece blackKing;
     
+    WhitePiece whitePawns[] = new WhitePiece[8];
     WhitePiece whiteRook[] = new WhitePiece[2];
     WhitePiece whiteKnight[] = new WhitePiece[2];
     WhitePiece whiteBishop[] = new WhitePiece[2];
@@ -13,6 +15,13 @@ public class Board {
     WhitePiece whiteKing;
     
     public Board(){
+        setBlacks();
+        setWhites();
+        setPawns();
+        
+    }  
+    
+    private void setBlacks(){        
         blackRook[0] = new BlackPiece(0, 0, Piece.rook);
         blackKnight[0] = new BlackPiece(1, 0, Piece.knight);
         blackBishop[0] = new BlackPiece(2, 0, Piece.bishop); 
@@ -22,10 +31,25 @@ public class Board {
         blackKnight[1] = new BlackPiece(6, 0, Piece.knight);
         blackRook[1] = new BlackPiece(7, 0, Piece.rook);
         
-    }    
+    }
+    private void setWhites(){        
+        whiteRook[0] = new WhitePiece(0, 7, Piece.rook);
+        whiteKnight[0] = new WhitePiece(1, 7, Piece.knight);
+        whiteBishop[0] = new WhitePiece(2, 7, Piece.bishop); 
+        whiteQueen = new WhitePiece(3, 7, Piece.queen);
+        whiteKing = new WhitePiece(4,7, Piece.king);
+        whiteBishop[1] = new WhitePiece(5, 7,Piece.bishop);                
+        whiteKnight[1] = new WhitePiece(6, 7, Piece.knight);
+        whiteRook[1] = new WhitePiece(7, 7, Piece.rook);
+    }
+    private void setPawns(){
+        for (int i = 0; i < Piece.board.length; i++){
+            blackPawns[i] = new BlackPiece(i, 1, Piece.pawn);
+            whitePawns[i] = new WhitePiece(i, 6, Piece.pawn);
+        }
+    }
     
     public static void displayBoard(){
-        System.out.println(Piece.board[0].length);
         
         boolean whiteSquare = true;
         for (int l = 0; l < Piece.board.length; l++) {
@@ -36,8 +60,10 @@ public class Board {
                 } else {
                     System.out.print(" " + Piece.board[w][l] + " ");
                 }
+                //alternates empty grid
                 whiteSquare = !whiteSquare;
-            }
+            }            
+            //restets white squares at end of line to create alternating grid
             whiteSquare = !whiteSquare;
             System.out.println();
         }
